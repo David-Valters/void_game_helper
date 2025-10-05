e5e43d304dd0ff8fa5a9402f8caaa41f0c329094
duck <->
2025-10-05 23:02:10 +0300

    Update starting_status to use config value for starting message

--- action.py
+++ action.py
@@ -27,7 +27,7 @@ def starting_status(img):
     if not 'NO_SEND' in config:
         img = get_wait_time_window(img)
         if img:
-            mes = "🟢Starting..."
+            mes = config.get("starting_message", "🟢Starting...")
             bot.send_photo(chat_id=config["chat_id"], photo=img, message_thread_id=config.get("message_thread_id"), caption=mes)
         else:
             bot.send_sticker(chat_id=config["chat_id"], sticker=config["starting_sticker_id"], message_thread_id=config.get("message_thread_id"))
